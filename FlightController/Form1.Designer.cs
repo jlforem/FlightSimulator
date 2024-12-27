b5f39a848fb0a54aafc05c024ce81f7e4f72bfbf
jlform <jordanforeman1@gmail.com>
2024-12-27 19:39:42 +0000

    replace

---
+++ FlightController/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FlightController
+{
+    partial class dataGrid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectButton = new System.Windows.Forms.Button();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.ipAddressTextBox = new System.Windows.Forms.TextBox();
+            this.pitchTrackBar = new System.Windows.Forms.TrackBar();
+            this.throttleTrackBar = new System.Windows.Forms.TrackBar();
+            this.pitchLabel = new System.Windows.Forms.Label();
+            this.throttleLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.warningTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pitchTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.throttleTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(190, 10);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(118, 12);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(66, 20);
+            this.portTextBox.TabIndex = 1;
+            this.portTextBox.Text = "enter port";
+            // 
+            // ipAddressTextBox
+            // 
+            this.ipAddressTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ipAddressTextBox.Name = "ipAddressTextBox";
+            this.ipAddressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ipAddressTextBox.TabIndex = 2;
+            this.ipAddressTextBox.Text = "enter ip";
+            // 
+            // pitchTrackBar
+            // 
+            this.pitchTrackBar.Location = new System.Drawing.Point(13, 174);
+            this.pitchTrackBar.Name = "pitchTrackBar";
+            this.pitchTrackBar.Size = new System.Drawing.Size(224, 45);
+            this.pitchTrackBar.TabIndex = 3;
+            // 
+            // throttleTrackBar
+            // 
+            this.throttleTrackBar.Location = new System.Drawing.Point(13, 225);
+            this.throttleTrackBar.Name = "throttleTrackBar";
+            this.throttleTrackBar.Size = new System.Drawing.Size(224, 45);
+            this.throttleTrackBar.TabIndex = 4;
+            // 
+            // pitchLabel
+            // 
+            this.pitchLabel.AutoSize = true;
+            this.pitchLabel.Location = new System.Drawing.Point(243, 174);
+            this.pitchLabel.Name = "pitchLabel";
+            this.pitchLabel.Size = new System.Drawing.Size(30, 13);
+            this.pitchLabel.TabIndex = 5;
+            this.pitchLabel.Text = "pitch";
+            // 
+            // throttleLabel
+            // 
+            this.throttleLabel.AutoSize = true;
+            this.throttleLabel.Location = new System.Drawing.Point(243, 225);
+            this.throttleLabel.Name = "throttleLabel";
+            this.throttleLabel.Size = new System.Drawing.Size(39, 13);
+            this.throttleLabel.TabIndex = 6;
+            this.throttleLabel.Text = "throttle";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(312, 225);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(476, 211);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // warningTextBox
+            // 
+            this.warningTextBox.Location = new System.Drawing.Point(546, 199);
+            this.warningTextBox.Name = "warningTextBox";
+            this.warningTextBox.ReadOnly = true;
+            this.warningTextBox.Size = new System.Drawing.Size(242, 20);
+            this.warningTextBox.TabIndex = 8;
+            this.warningTextBox.Text = "Warning: None";
+            // 
+            // dataGrid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.warningTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.throttleLabel);
+            this.Controls.Add(this.pitchLabel);
+            this.Controls.Add(this.throttleTrackBar);
+            this.Controls.Add(this.pitchTrackBar);
+            this.Controls.Add(this.ipAddressTextBox);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.connectButton);
+            this.Name = "dataGrid";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pitchTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.throttleTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.TextBox ipAddressTextBox;
+        private System.Windows.Forms.TrackBar pitchTrackBar;
+        private System.Windows.Forms.TrackBar throttleTrackBar;
+        private System.Windows.Forms.Label pitchLabel;
+        private System.Windows.Forms.Label throttleLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox warningTextBox;
+    }
+}
+
---
+++ FlightController/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web.Script.Serialization;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace FlightController
+{
+    public partial class dataGrid : Form
+    {
+        public dataGrid()
+        {
+            InitializeComponent();
+        }
+        TcpClient client;
+        Thread telemetryThread = null;
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ipAddress = ipAddressTextBox.Text;
+                int port = int.Parse(portTextBox.Text);
+
+                TcpClient client = new TcpClient(ipAddress, port);
+                byte[] data = Encoding.ASCII.GetBytes("Connection Successful");
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                data = Encoding.ASCII.GetBytes("Still Here");
+
+                while (client.Connected)
+                {
+                    stream.Write(data, 0, data.Length);
+                    Thread.Sleep(1000);
+                }
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", ex);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException: {0}", ex);
+            }
+
+            StartReceivingTelemetry();
+        }
+
+        struct ControlsUpdate
+        {
+            public double Throttle;
+            //Current throttle setting as a percentage (i.e. 0% no
+            //throttle, 100% full throttle).
+            public double ElevatorPitch;
+            //Current Elevator Pitch in degrees. Positive creates
+            //upwards lift, negative downwards.
+        }
+
+        struct TelemetryUpdate
+        {
+            public double Altitude { get; set;}
+            //Altitude in ft.
+            public double Speed { get; set; }
+            //Plane's speed in Knts.
+            public double Pitch { get; set; }
+            //Plane's pitch in degrees relative to horizon. Positive is planes pointing upwards, negative plane points downwards.
+            public double VerticalSpeed { get; set; }
+            //Plane's vertical speed in Feet per minute.
+            public double Throttle { get; set; }
+            //Current throttle setting as a percentage (i.e., 0% no throttle, 100% full throttle).
+            public double ElevatorPitch { get; set; }
+            //Current Elevator Pitch in degrees. Positive creates upwards lift, negative downwards.
+            public int WarningCode { get; set; }
+            //Warning code: 0 - No Warnings; 1 - Too Low (less than 1000ft); 2 - Stall.
+        }
+
+        private delegate void TelemetryUpdateDelegate(TelemetryUpdate telemetry);
+
+        private TelemetryUpdateDelegate telemetryUpdateDelegate;
+
+        private void UpdateTelemetry(TelemetryUpdate telemetry, byte[] buffer, int bytesRead)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            TelemetryUpdate parsedData = serializer.Deserialize<TelemetryUpdate>(data);
+
+            telemetry.Altitude = parsedData.Altitude;
+            telemetry.Speed = parsedData.Speed;
+            telemetry.Pitch = parsedData.Pitch;
+            telemetry.VerticalSpeed = parsedData.VerticalSpeed;
+            telemetry.Throttle = parsedData.Throttle;
+            telemetry.ElevatorPitch = parsedData.ElevatorPitch;
+            telemetry.WarningCode = parsedData.WarningCode;
+        }
+
+        private void ReceiveTelemetryData()
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[256];
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    // Process the received telemetry data
+                    // Example code:
+                    JavaScriptSerializer serializer = new JavaScriptSerializer();
+                    TelemetryUpdate telemetry = serializer.Deserialize<TelemetryUpdate>(data);
+                    telemetryUpdateDelegate(telemetry);
+                    UpdateTelemetry(telemetry);
+                }
+
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex);
+            }
+        }
+
+        private void StartReceivingTelemetry()
+        {
+            // Start a new thread to receive telemetry data
+            Thread telemetryThread = new Thread(ReceiveTelemetryData);
+            telemetryThread.Start();
+        }
+
+        private void RemoteFlightController_Load(object sender, EventArgs e)
+        {
+            // Assign the UpdateTelemetry method to the telemetryUpdateDelegate
+            telemetryUpdateDelegate = UpdateTelemetry;
+
+            // Start receiving telemetry data
+            StartReceivingTelemetry();
+        }
+
+        private void warningHandler(int warningCode)
+        {
+            if (warningCode == 1)
+            {
+                warningTextBox.Text = "Warning: Too Low";
+            }
+            else if (warningCode == 2)
+            {
+                warningTextBox.Text = "Warning: Stall";
+            }
+            else if (warningCode == 0)
+            {
+                warningTextBox.Text = "No Warnings";
+            }
+        }
+    }
+}
